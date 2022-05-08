a4d3f74d1d3c6290659b379dc88b92d74b500858
Brenton Adey <brentonadey@gmail.com>
2022-05-08 08:29:19 +0100

    Added PWM output with LED

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -7,13 +7,22 @@ GPIO.setmode(GPIO.BOARD)
 #set GPIO Pins
 SENSOR_TRIGGER = 18
 SENSOR_ECHO = 24
+
+LED_PIN = 12
  
 #set GPIO direction (IN / OUT)
 GPIO.setup(SENSOR_TRIGGER, GPIO.OUT)
 GPIO.setup(SENSOR_ECHO, GPIO.IN)
 
+GPIO.setup(LED_PIN, GPIO.OUT)  # Set GPIO pin 12 to output mode.
+pwm = GPIO.PWM(LED_PIN, 100)   # Initialize PWM on pwmPin 100Hz frequency
+
 # Ensure first reading is clean
 GPIO.output(SENSOR_TRIGGER, GPIO.LOW)
+
+# Set duty cycle variable to 0 for 0%
+dc=0
+pwm.start(dc)
  
 def distance():
     # set Trigger to HIGH
@@ -46,9 +55,14 @@ try:
     while True:
         dist = distance()
         print ("Measured Distance = %.1f cm" % dist)
+        if dist > 50:
+            dc = 0
+        else:
+            dc = 100 - 2*dist
+        pwm.ChangeDutyCycle(dc)
         time.sleep(1)
- 
     # Reset by pressing CTRL + C
 except KeyboardInterrupt:
     print("Measurement stopped by User")
+    pwm.stop()
     GPIO.cleanup()
